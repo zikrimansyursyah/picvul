b1b79191830de7788497b25a404ae4541c6afbf4
Zikri Mansyursyah <zikrimansyursyah@gmail.com>
2021-12-31 19:20:13 +0700

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
